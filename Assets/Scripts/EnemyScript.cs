8e32e9bc4cb7cd0eb70bfbfd10d07b79acc53c77
Timothy Su <ts63280@gmail.com>
2023-08-05 22:38:31 -0700

    panic mode

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+    public Rigidbody2D rb;
     public bool Alive;
+    public bool Panic;
+    public float moveSpeed;
+    public Vector2 moveDirection;
     // Start is called before the first frame update
     void OnTriggerEnter2D(Collider2D collision)
     {
+        if (!Panic){
+            Debug.Log("Panic?" + Panic);
+            GameObject[] gos;
+            gos = GameObject.FindGameObjectsWithTag("NPC");
+
+            foreach(GameObject go in gos){
+                go.SendMessage("StartPanic");
+            }
+        }
         if (Alive){
             if (collision.gameObject.name == "Square"){
                 Debug.Log("died");
@@ -18,11 +31,28 @@ public class EnemyScript : MonoBehaviour
     void Start()
     {
         Alive = true;
+        Panic = false;
+        rb = rb != null ? rb : Global.FindComponent<Rigidbody2D>(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
+    }
 
+    void FixedUpdate()
+    {
+        if (Panic && Alive){
+            Debug.Log("moving" + moveDirection + moveSpeed);
+            rb.AddForce(moveDirection * moveSpeed);
+        }
+    }
+
+    void StartPanic()
+    {
+        Panic = true;
+        moveSpeed = 10;
+        moveDirection = Random.insideUnitCircle.normalized;
+        Debug.Log("Panicking" + Panic);
     }
 }
