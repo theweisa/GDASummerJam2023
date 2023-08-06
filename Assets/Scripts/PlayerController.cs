e5571ecfe2d23834355c8a3984748c89b9f73273
Timothy Su <ts63280@gmail.com>
2023-08-05 17:04:39 -0700

    Punch basics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     [Header("Player Parameters")]
     [Space(4)]
     public Rigidbody2D rb;
+    public Collider2D punchHitbox;
     public float moveSpeed;
 
     [Header("Run-time variables")]
@@ -16,18 +17,19 @@ public class PlayerController : MonoBehaviour
 
     void Awake() {
         rb = rb != null ? rb : Global.FindComponent<Rigidbody2D>(gameObject);
+        punchHitbox.enabled = false;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void FixedUpdate() {
@@ -36,5 +38,22 @@ public class PlayerController : MonoBehaviour
 
     void OnMove(InputValue value) {
         moveDirection = value.Get<Vector2>();
+    }
+
+    void OnFire(InputValue value){
+        StartCoroutine(punch());
+    }
+
+    IEnumerator punch(){
+        Vector3 positionMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 towardsMouseFromPlayer = positionMouse - transform.position;
+        towardsMouseFromPlayer = towardsMouseFromPlayer.normalized;
+        punchHitbox.transform.position = transform.position;
+        punchHitbox.transform.position = Vector3.MoveTowards(punchHitbox.transform.position, punchHitbox.transform.position + towardsMouseFromPlayer * 6, 16);
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + towardsMouseFromPlayer * 2, 16);
+        punchHitbox.enabled = true;
+        Debug.Log("Punching");
+        yield return new WaitForSeconds(.5f);
+        punchHitbox.enabled = false;
     }
 }
